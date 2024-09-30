77113acc86b2afcd96e3f62e15d474392f6f9660
iongion <stoica.ionut@gmail.com>
2024-09-30 13:25:06 +0200

    Release version 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # container-desktop-wsl-relay
-Relay WSL unix sockets through windows named pipes
+
+Relay WSL unix sockets through windows named pipes by using a relay proxy native windows program.
+
+## Why
+
+- Although `AF_UNIX` support exists in Window, Windows native programs cannot use unix sockets from WSL 2.
+- Spawning TCP listeners on `localhost` would be easy, but it is not secure as any users logged-in to the machine can listen to them. In some environments, this might not be acceptable.
+
+## Using the named pipe
+
+Flow of communication
+
+```mermaid
+sequenceDiagram
+    Client->>+WSL: Start wsl.exe linux native container-desktop-relay
+    WSL-->>UnixSocket: Listen to UNIX socket(loop forever until able to connect)
+    UnixSocket<<-->>WSL: Bidirectional communication
+    WSL-->>Windows: Spawn native windows container-desktop-relay.exe
+    Windows-->>NamedPipe: Listen to named pipe and connect stdin and stdout to named pipe
+    NamedPipe<<-->>UnixSocket: Bidirectional communication (unix socket <=> named pipe)
+    Client<<-->>NamedPipe: Client can initiate bidirectional communicate with named pipe
+    NamedPipe<<-->>UnixSocket: Relay between named pipe and unix socket
+```
+
+## Requirements
+
+- NodeJS installed in windows and exposed to `%PATH%` (needed to test the relay named pipe connection)
+- WSL with default distribution(Ubuntu)
+- Build dependencies
+
+In the default WSL distribution(Ubuntu), where the repo is cloned and where tests are made, golang and windows cross-compilation and optimization tools are needed.
+
+```bash
+sudo apt-get install build-essential gcc-mingw-w64 musl-tools golang upx-ucl
+```
+
+## Building
+
+- Static binaries are generated to make the relay available on as many linux distributions as possible, besides the default WSL distribution(Ubuntu)
+- UPX is used to reduce the size of the binaries, that are usually embedded into other programs
+
+```bash
+./relay-build.sh
+```
+
+## Testing
+
+- Inside the cloned directory, execute the next script from `wsl.exe` terminal console - **IMPORTANT - Not windows powershell or cmd!**
+- Must use `node.exe` because `relay-test.js` will need to connect to a named pipe, only Windows has named pipes, hence `node.exe`, not `node`!
+
+```bash
+./relay-build.sh
+npm install
+node.exe relay-test.js
+```
+
+## Permissions
+
+- **AllowEveryone** = `"S:(ML;;NW;;;LW)D:(A;;0x12019f;;;WD)"` - _AllowEveryone - **to be avoided**, allows any users running on current machine._
+- **AllowCurrentUser** = `"D:P(A;;GA;;;$SID)"` - _AllowCurrentUser grants full access permissions for the current user. The variable `$SID` is interpolated at runtime._
+- **AllowServiceSystemAdmin** = `"D:(A;ID;FA;;;SY)(A;ID;FA;;;BA)(A;ID;FA;;;LA)(A;ID;FA;;;LS)"` - _AllowServiceSystemAdmin grants full access permissions for Service, System, Administrator group and account._
+
+## Usage
+
+From a WSL terminal console
+
+```shell
+export RELAY_SOCKET="/var/run/docker.sock"
+export RELAY_PIPE="\\\\.pipe\\container-desktop-relay-docker"
+./container-desktop-wsl-relay --socket "$RELAY_SOCKET" --pipe "$RELAY_PIPE" --relay-program-path "$PROJECT_HOME/container-desktop-wsl-relay.exe"
+```
+
+Using a node-js child_process started from Windows
+
+```shell
+node.exe relay-test.js
+```
+
+## Pitfalls
+
+As of version `1.0.1` I am unable to ensure that the spawned windows native executable dies when parent WSL linux native spawner gets killed or exits, I am looking for options.
---
+++ relay-test.js
@@ -0,0 +1,147 @@
+const { spawn } = require("node:child_process");
+const http = require('http');
+const axios = require("axios");
+const adapter = require("axios/lib/adapters/http"); // This doesn't work in the browser
+
+const UNIX_SOCKET = "/var/run/docker.sock";
+const NAMED_PIPE = "\\\\.\\pipe\\container-desktop-wsl-relay-test";
+
+let processExited = false;
+let child;
+
+async function pingSocket(namedPipe) {
+    const statusConfig = {
+        socketPath: namedPipe,
+        baseURL: "http://d",
+        adapter
+    };
+    console.debug("Checking status of relay server");
+    const driver = axios.create(statusConfig);
+    const response = await driver.get("/_ping");
+    console.debug("Response received", response.status, response.data);
+    return response.status === 200;
+}
+
+async function exec_buffered(hostLauncher, commandLine, onChunk, onExit, onError) {
+    return await new Promise((resolve, reject) => {
+        console.debug("Spawning WSL process:", [hostLauncher, ...commandLine].join(" "));
+        child = spawn(hostLauncher, commandLine, {
+            shell: false,
+            windowsHide: false,
+            detached: true,
+            stdio: ["inherit", "pipe", "pipe"]
+        });
+        child.stdout.setEncoding("utf8");
+        child.stderr.setEncoding("utf8");
+        child.stdout.on("data", function (data) {
+            const chunk = Buffer.from(data);
+            onChunk?.(chunk);
+        });
+        child.stderr.on("data", function (data) {
+            const chunk = Buffer.from(data);
+            onChunk?.(chunk);
+        });
+        child.on("exit", function (code) {
+            console.debug("Child process exited", code);
+            try {
+                if (child.stdin) {
+                    console.debug("Ending child process stdin");
+                    child.stdin.end();
+                }
+            } catch (error) {
+                console.error("child process stdin end error", error);
+            }
+            onExit?.({
+                //
+                pid: child.pid,
+                exitCode: child.exitCode,
+                command: [hostLauncher, ...commandLine].join(" ")
+            });
+        });
+        child.on("error", function (error) {
+            console.error("child process error", error);
+            onError?.(error)
+        });
+        resolve({
+            //
+            pid: child.pid,
+            exitCode: child.exitCode,
+            command: [hostLauncher, ...commandLine].join(" ")
+        });
+    });
+}
+
+
+
+async function main() {
+    // Spawn
+    const relayProgramPath = "./container-desktop-wsl-relay.exe"; // If spawned from windows, this path must be converted using wslpath
+    try {
+        const spawned = await exec_buffered(
+            // WSL session with default distribution
+            "wsl.exe",
+            [
+                "--exec",
+                // Relay program
+                "./container-desktop-wsl-relay",
+                `--socket=${UNIX_SOCKET}`,
+                `--pipe=${NAMED_PIPE}`,
+                `--relay-program-path=${relayProgramPath}`
+            ],
+            // Chunk handler
+            (chunk) => {
+                console.debug("Chunk received", chunk.toString());
+            },
+            // Exit handler
+            () => {
+                // processExited = true;
+            }
+        );
+        console.debug("Relay started", spawned);
+        let isRequesting = false;
+        const iid = setInterval(async () => {
+            if (processExited) {
+                console.debug("Child exited");
+                clearInterval(iid);
+            }
+            if (isRequesting) {
+                console.debug("A request is already in progress");
+                return;
+            }
+            try {
+                console.debug(`Issuing ping request to ${NAMED_PIPE} <==> ${UNIX_SOCKET}`);
+                isRequesting = true;
+                await pingSocket(NAMED_PIPE);
+            } catch (error) {
+                console.debug("Request error", error.message);
+            } finally {
+                isRequesting = false;
+            }
+        }, 1500)
+    } catch (error) {
+        console.error("Unable to spawn", error);
+    }
+}
+
+
+function signalHandler(signal) {
+    console.debug("Received exit signal", signal);
+    if (child) {
+        try {
+            console.debug("Killing child", child.pid);
+            // process.kill(child.pid, "SIGTERM");
+            child.kill();
+            child.unref();
+        } catch (error) {
+            console.error("Unable to kill child", error);
+        }
+    } else {
+        console.debug("No child to kill");
+    }
+    process.exit(0);
+}
+process.on('SIGINT', signalHandler);
+process.on('SIGTERM', signalHandler);
+process.on('SIGQUIT', signalHandler);
+
+main();
